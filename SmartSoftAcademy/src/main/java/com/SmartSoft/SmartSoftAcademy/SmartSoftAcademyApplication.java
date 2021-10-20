dc7cd4ecb67e8c679a1af6445d9518302e53c8b4
201611779@est.umss.edu <gutierreztorrezjose@gmail.com>
2021-10-20 12:39:59 -0400

    Se termino de realizar la interfaz

---
+++ SmartSoftAcademy/src/main/java/com/SmartSoft/SmartSoftAcademy/SmartSoftAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.SmartSoft.SmartSoftAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSoftAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSoftAcademyApplication.class, args);
+	}
+
+}
---
+++ SmartSoftAcademy/src/test/java/com/SmartSoft/SmartSoftAcademy/SmartSoftAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartSoft.SmartSoftAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSoftAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
